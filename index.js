5585b99c341b6510fa6ff131fd4542045c375b4d
Mohit kumar paul <mohit1paul@gmail.com>
2024-04-27 01:28:59 +0530

    project done

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const incre = document.querySelector()
+let currentVal = 0;
+
+const title = document.querySelector('#title')
+const decrementBtn = document.querySelector('#decrement')
+const resetBtn = document.querySelector('#Reset')
+const incrementBtn = document.querySelector('#Increment')
+
+
+incrementBtn.addEventListener('click', () => {
+        currentVal ++;
+        title.textContent = currentVal;
+    
+});
+
+decrementBtn.addEventListener('click', () => {
+    currentVal --;
+    title.textContent = currentVal;
+
+});
+
+
+resetBtn.addEventListener('click', () => {
+    currentVal = 0;
+    title.textContent = currentVal;
+
+});
